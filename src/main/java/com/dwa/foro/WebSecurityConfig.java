2c71377c124dd9bd01e4e469f8b2430c9ee388d0
Jonhy <Jonhy@JONHY>
2021-05-31 21:14:13 -0500

    creacion de ficheros

---
+++ src/main/java/com/dwa/foro/WebSecurityConfig.java
@@ -0,0 +1,34 @@
+package com.dwa.foro;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception{
+		http
+		.authorizeRequests()
+		  .antMatchers( //Rutas con acceso libre
+				  "/",
+				  "/css/**",
+				  "/js/**",
+				  "/usuario/**",
+				  "/usuario/validar"
+				  ).permitAll()
+		  .anyRequest().authenticated() //Cualquier otra ruta es con token
+		  .and()
+		.formLogin()
+			.loginPage("/usuario/validar") //Esta ruta se usa para validar al usuario Accion VALIDAR  GET 
+			.successForwardUrl("/usuario/validar") //Accion validar POST
+			.permitAll()
+			.and()
+		.logout()
+		   .permitAll();
+	}
+}
